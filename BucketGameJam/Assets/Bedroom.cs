2ccc72c71e1e4aa1d93185deec95d9eb15993a8b
Kylar Black <kylar.c.black@gmail.com>
2022-01-28 22:02:08 -0600

    Base Bedroom
    First proto of the player's room

---
+++ BucketGameJam/Assets/Bedroom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bedroom : MonoBehaviour
+{
+    public void Mirror()
+    {
+
+    }
+
+    public void Window()
+    {
+
+    }
+
+    public void Journal()
+    {
+
+    }
+
+    public void Save()
+    {
+
+    }
+
+    public void Transition()
+    {
+
+    }
+}
---
+++ BucketGameJam/Assets/Mirror.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    
+    public void ClickMirror()
+    {
+
+    }
+
+    void Destroy()
+    {
+        
+    }
+
+}
