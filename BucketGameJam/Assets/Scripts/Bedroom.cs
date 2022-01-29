e1d84506f1e7e283a922c62ea0f6579f25566b2a
Kylar Black <kylar.c.black@gmail.com>
2022-01-29 02:00:21 -0600

    GitIgnore and mirror functionality

--- BucketGameJam/Assets/Scripts/Bedroom.cs
+++ BucketGameJam/Assets/Scripts/Bedroom.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Bedroom : MonoBehaviour
 {
     public GameObject mirrorReflection;
+    GameManager gm;
 
+    private void Start()
+    {
+        Debug.Log(GameManager.gm);
+        mirrorReflection = FindObjectOfType<GameManager>().gameObject.GetComponent<GameManager>().mirrorReflection;
+        //Debug.Log(GameManager.gm.gameObject);
+    }
 
     public void Mirror()
     {
         //Show reflection of player's current state
-        
+        GameObject mirror = Instantiate(mirrorReflection, gameObject.transform.position, Quaternion.identity, gameObject.transform);
     }
 
     public void Journal()
