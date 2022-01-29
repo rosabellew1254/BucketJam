e7f02886fee8f2297561b439b79ac677815314f4
r1ckl3ss <rickjherrera91@gmail.com>
2022-01-29 01:41:41 -0600

    Made main menu script

---
+++ BucketGameJam/Assets/Scripts/cMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cMainMenu : MonoBehaviour
+{
+    GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.gm;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
