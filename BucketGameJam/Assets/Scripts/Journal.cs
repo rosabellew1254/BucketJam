f9c25e3628ddbee7cb88754e16a6c286d0cf947f
Mettler7x <dannyjdiep@gmail.com>
2022-01-29 18:53:23 -0600

    Journal
    got the things working

--- BucketGameJam/Assets/Scripts/Journal.cs
+++ BucketGameJam/Assets/Scripts/Journal.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Journal : MonoBehaviour
 {
-   public void JournalClose()
+    public string[] Journalpages;
+    public int currentpage = 0;
+    public void displayPage(int page)
     {
-        Object.Destroy(gameObject);
+        gameObject.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = Journalpages[page];
     }
     public void LeftPageClick()
     {
-
+        currentpage = Mathf.Clamp(currentpage - 1, 0, Journalpages.Length);
+        displayPage(currentpage);
     }
     public void RightPageClick()
     {
+        currentpage = Mathf.Clamp(currentpage + 1, 0, Journalpages.Length);
+        displayPage(currentpage);
+    }
 
+    public void JournalClose()
+    {
+        Object.Destroy(gameObject);
     }
 }
