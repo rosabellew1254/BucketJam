c9b44601c5766424ce148ce5e0d9e4ff942a9336
Kylar Black <kylar.c.black@gmail.com>
2022-02-03 12:42:20 -0600

    SisterPrefab and script

---
+++ BucketGameJam/Assets/Scripts/Sister.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sister : MonoBehaviour
+{
+    Image textBox;
+    Text textBoxFill;
+
+
+
+    //create funtions for button dialogs if there is going to be any of that
+    //make an array for each set of converstations
+
+
+
+    public void TextDialog()
+    {
+
+    }
+
+}
