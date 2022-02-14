939231765c4fc1ee273f1f929946e5884f870790
jakesullivan22 <74078285+jakesullivan22@users.noreply.github.com>
2022-02-14 12:57:34 -0600

    fixed cursor bug

--- BucketGameJam/Assets/Scripts/CursorScript.cs
+++ BucketGameJam/Assets/Scripts/CursorScript.cs
@@ -3,35 +3,22 @@ using UnityEngine.UI;
 
 public class CursorScript : MonoBehaviour
 {
-    Image image;
     public Sprite normalSprite;
     public Sprite eldritchSprite;
-    public static CursorScript cursor;
     GameManager gm;
-    Camera mainCam;
     
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
-        cursor = this;
         gm = GameManager.gm;
-        image = GetComponent<Image>();
-    }
-
-    private void OnEnable()
-    {
-        mainCam = Camera.main;
-    }
-
-    private void OnLevelWasLoaded(int level)
-    {
-        mainCam = Camera.main;
     }
 
     public void UpdateSprite()
     {
-        image.sprite = gm.worldState == GameManager.state.largeEvil ? eldritchSprite : normalSprite;
+        Image myImage = GetComponent<Image>();
+        int curSanity = FindObjectOfType<PlayerController>().curSanity;
+        myImage.sprite = curSanity < 0 ? eldritchSprite : normalSprite;
     }
 
     void Update()
